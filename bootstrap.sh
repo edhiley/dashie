f7ad099043e301e40a03a0466987d503390c7dd5
edhiley <edhiley@gmail.com>
2015-02-11 00:51:22 +0000

    added nodejs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+curl -sL https://deb.nodesource.com/setup | sudo bash -
 apt-get update
 apt-get install -y git python-pip nodejs
 apt-get install -y language-pack-en
 pip install virtualenv
+npm install http-server -g
+
 
 pushd /home/vagrant
 su vagrant
@@ -16,3 +19,6 @@ pip install -r requirements.txt
 cp /vagrant/scripts/start.sh /home/vagrant/pydashie/
 chown vagrant /home/vagrant/pydashie/start.sh
 
+
+cd /home/vagrant/pydashie/examples
+http-server . &
